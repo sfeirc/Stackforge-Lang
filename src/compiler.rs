4074d76e2e810578654594fcf9b0dfe57f972ce5
Clovis SFEIR <clovis.sfeir.cs@gmail.com>
2026-08-14 10:10:00 +0000

    feat(compiler): statement compilation

--- src/compiler.rs
+++ src/compiler.rs
@@ -72,3 +72,67 @@ impl<'a> FnCompiler<'a> {
             depth: self.scope_depth,
         });
     }
+
+    fn compile_block(&mut self, stmts: &[Stmt]) -> Result<(), SfError> {
+        for s in stmts {
+            self.compile_stmt(s)?;
+        }
+        Ok(())
+    }
+
+    fn compile_scoped_block(&mut self, stmts: &[Stmt], line: u32) -> Result<(), SfError> {
+        self.begin_scope();
+        self.compile_block(stmts)?;
+        self.end_scope(line);
+        Ok(())
+    }
+
+    fn compile_stmt(&mut self, stmt: &Stmt) -> Result<(), SfError> {
+        match stmt {
+            Stmt::Let(name, expr, _line) => {
+                self.compile_expr(expr)?;
+                self.declare_local(name);
+            }
+            Stmt::ExprStmt(expr) => {
+                self.compile_expr(expr)?;
+                self.chunk.emit(OpCode::Pop, 0);
+            }
+            Stmt::Print(exprs, line) => {
+                for e in exprs {
+                    self.compile_expr(e)?;
+                }
+                self.chunk.emit(OpCode::Print(exprs.len() as u8), *line);
+            }
+            Stmt::If(cond, then_b, else_b) => {
+                self.compile_expr(cond)?;
+                let else_jump = self.chunk.emit(OpCode::JumpIfFalse(0), 0);
+                self.compile_scoped_block(then_b, 0)?;
+                let end_jump = self.chunk.emit(OpCode::Jump(0), 0);
+                let else_target = self.chunk.here();
+                self.chunk.patch_jump(else_jump, else_target);
+                self.compile_scoped_block(else_b, 0)?;
+                let end_target = self.chunk.here();
+                self.chunk.patch_jump(end_jump, end_target);
+            }
+            Stmt::While(cond, body) => {
+                let loop_start = self.chunk.here();
+                self.compile_expr(cond)?;
+                let exit_jump = self.chunk.emit(OpCode::JumpIfFalse(0), 0);
+                self.compile_scoped_block(body, 0)?;
+                self.chunk.emit(OpCode::Jump(loop_start), 0);
+                let exit_target = self.chunk.here();
+                self.chunk.patch_jump(exit_jump, exit_target);
+            }
+            Stmt::Block(stmts) => self.compile_scoped_block(stmts, 0)?,
+            Stmt::Return(expr, line) => {
+                match expr {
+                    Some(e) => self.compile_expr(e)?,
+                    None => {
+                        self.chunk.emit(OpCode::PushNil, *line);
+                    }
+                }
+                self.chunk.emit(OpCode::Return, *line);
+            }
+        }
+        Ok(())
+    }
