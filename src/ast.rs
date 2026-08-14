2aed9c91553a605b5e393ff41b1105d0e51b6c2b
Clovis SFEIR <clovis.sfeir.cs@gmail.com>
2026-08-14 08:53:00 +0000

    feat(ast): expression node types

---
+++ src/ast.rs
@@ -0,0 +1,47 @@
+/// Abstract syntax tree produced by the parser. Shared verbatim by both
+/// execution backends: the tree-walking interpreter evaluates it directly,
+/// and the bytecode compiler walks it once to emit `OpCode`s.
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    NotEq,
+    Lt,
+    LtEq,
+    Gt,
+    GtEq,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LogicOp {
+    And,
+    Or,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnOp {
+    Neg,
+    Not,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Number(f64),
+    Str(String),
+    Bool(bool),
+    Nil,
+    Ident(String, u32),
+    Array(Vec<Expr>),
+    Map(Vec<(String, Expr)>),
+    Index(Box<Expr>, Box<Expr>, u32),
+    Unary(UnOp, Box<Expr>, u32),
+    Binary(BinOp, Box<Expr>, Box<Expr>, u32),
+    Logical(LogicOp, Box<Expr>, Box<Expr>),
+    Assign(String, Box<Expr>, u32),
+    IndexAssign(Box<Expr>, Box<Expr>, Box<Expr>, u32),
+    Call(String, Vec<Expr>, u32),
+}
